5f34a0f214c31e36bc660adfb805e77d0abf0bd7
Jorge Correa Gutiérrez <jorgecgjr@hotmail.com>
2025-04-12 08:52:20 -0600

    Estudio Sábado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/prueba', function () {
+    return "Esta es una ruta de pruebas";
 });
+
+Route::get('/{variable}/{categoria?}', function ($variable, $categoria = null){
+
+    if ($categoria)
+    {
+        return "Estamos en la ruta $variable de categoria: $categoria";
+    }
+    return "Estamos en la ruta $variable sin categoria";
+});
+
+Route::get('/{variable}', function ($variable){
+    return "Estamos en la ruta $variable";
+});
+
+
